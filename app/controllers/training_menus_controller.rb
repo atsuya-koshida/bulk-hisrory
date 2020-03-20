494e15ed48ab20c0550be8b03edac598884f70dd
atsuya-koshida <59789801+atsuya-koshida@users.noreply.github.com>
2020-03-20 17:48:56 +0900

    トレーニングメニューのテーブルを作成 (#2)
    * create training_menu.rb
    
    * create training_menu table
    
    * install active_hash
    
    * create part.rb on active_hash
    
    * install haml-rails
    
    * change sign_up view
    
    * add validation to training_menu.rb
    
    * create controller training_menus

---
+++ app/controllers/training_menus_controller.rb
@@ -0,0 +1,6 @@
+class TrainingMenusController < ApplicationController
+
+  def index
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_071943) do
+ActiveRecord::Schema.define(version: 2020_03_20_073458) do
+
+  create_table "training_menus", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "part_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_training_menus_on_title"
+    t.index ["user_id"], name: "index_training_menus_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -26,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_03_20_071943) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "training_menus", "users"
 end
