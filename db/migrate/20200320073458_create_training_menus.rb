81ead99527cf43c769b03f8bbf643ad458c628b0
atsuyakoshida <koshi0845@gmail.com>
2020-03-20 16:57:36 +0900

    create training_menu table

--- db/migrate/20200320073458_create_training_menus.rb
+++ db/migrate/20200320073458_create_training_menus.rb
@@ -1,8 +1,12 @@
 class CreateTrainingMenus < ActiveRecord::Migration[5.2]
   def change
     create_table :training_menus do |t|
-
+      t.string :title,    null: false
+      t.integer :part_id, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
+
+    add_index :training_menus, :title
   end
 end
