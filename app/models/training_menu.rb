9b5ec6f508f19ee9149a8b75e9e740b6b6cf8ee8
atsuyakoshida <koshi0845@gmail.com>
2020-03-20 17:18:08 +0900

    add validation to training_menu.rb

--- app/models/training_menu.rb
+++ app/models/training_menu.rb
@@ -1,4 +1,8 @@
 class TrainingMenu < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :part
+
+  validates :title, presence: true
+  validates :part_id, presence: true
+  validates :user_id, presence: true
 end
