348224f485a75185b9cc9f670a19c1efb65e78d6
atsuyakoshida <koshi0845@gmail.com>
2020-03-23 19:15:26 +0900

    add training_menus#new

--- app/controllers/training_menus_controller.rb
+++ app/controllers/training_menus_controller.rb
@@ -1,6 +1,27 @@
 class TrainingMenusController < ApplicationController
 
   def index
-    
+    @training_menus = TrainingMenu.all
+  end
+
+  def new
+    @training_menu = TrainingMenu.new
+  end
+
+  def create
+    @training_menu = TrainingMenu.new(training_menu_params)
+    binding.pry
+    if @training_menu.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+  def training_menu_params
+    params.require(:training_menu).permit(:title, :part_id).merge(user_id: current_user.id)
   end
 end
--- app/models/training_menu.rb
+++ app/models/training_menu.rb
@@ -1,6 +1,7 @@
 class TrainingMenu < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :part
+  belongs_to :user
 
   validates :title, presence: true
   validates :part_id, presence: true
