442a737d9bfe49699d130db0fa930b21b20b870e
atsuya-koshida <59789801+atsuya-koshida@users.noreply.github.com>
2020-03-24 18:12:30 +0900

    新規トレーニングメニューの作成・編集・削除機能を実装 (#4)
    * create training_menu.rb
    
    * create training_menu table
    
    * install active_hash
    
    * create part.rb on active_hash
    
    * install haml-rails
    
    * change sign_up view
    
    * add validation to training_menu.rb
    
    * create controller training_menus
    
    * partial header&footer
    
    * add training_menus#new
    
    * fix training_menus#new
    
    * add training_menus#destroy on ajax
    
    * add gem ransack
    
    * add workout-btn

--- app/assets/javascripts/training_menus.js
+++ app/assets/javascripts/training_menus.js
@@ -1,2 +1,10 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.delete').forEach(function(a) {
+    a.addEventListener('ajax:success', function() {
+      var td = a.parentNode;
+      var tr = td.parentNode;
+      tr.style.display = 'none';
+    });
+  });
+});
+
--- app/controllers/training_menus_controller.rb
+++ app/controllers/training_menus_controller.rb
@@ -1,6 +1,43 @@
 class TrainingMenusController < ApplicationController
-
+  before_action :set_training_menu, only: [:edit, :update, :destroy]
   def index
-    
+    @training_menus = TrainingMenu.all
+  end
+
+  def new
+    @training_menu = TrainingMenu.new
+  end
+
+  def create
+    @training_menu = TrainingMenu.new(training_menu_params)
+    if @training_menu.save
+      redirect_to training_menus_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @training_menu.update(training_menu_params)
+    redirect_to training_menus_path, notice: "種目「#{@training_menu.title}」を更新しました。"
+  end
+
+  def destroy
+    @training_menu.destroy
+    head :no_content
+  end
+
+
+
+  private
+  def training_menu_params
+    params.require(:training_menu).permit(:title, :part_id).merge(user_id: current_user.id)
+  end
+
+  def set_training_menu
+    @training_menu = current_user.training_menus.find(params[:id])
   end
 end
