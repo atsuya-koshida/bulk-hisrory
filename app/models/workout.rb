1872249a5621b054e98ca5beca04e145fe104053
atsuyakoshida <koshi0845@gmail.com>
2020-03-24 18:23:32 +0900

    add validation

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,2 +1,7 @@
 class Workout < ApplicationRecord
+  belongs_to :training_menu
+
+  validates :date, presence: true
+  validates :quality, presence: true
+  validates :training_menu_id, presence: true
 end
