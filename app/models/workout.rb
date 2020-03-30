4aa4c2fd75e1d487081453ebd79aff332d3240af
atsuya-koshida <59789801+atsuya-koshida@users.noreply.github.com>
2020-03-30 19:13:45 +0900

    複数のセットを保存したレコードの編集をできるように修正・種目のソート機能を追加 (#7) (#8)
    * 一度に複数のsetを記録できるように修正
    
    * fix search function
    
    * fix workout#edit function
    
    * fix training_menus #edit
    
    * fix index page
    
    * add sort function

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,10 +3,19 @@ class Workout < ApplicationRecord
   belongs_to_active_hash :quality
   belongs_to :training_menu
   has_many :workout_details, dependent: :destroy
-  accepts_nested_attributes_for :workout_details, allow_destroy: true
+  accepts_nested_attributes_for :workout_details, reject_if: :reject_both_blank, allow_destroy: true
 
   validates :date, presence: true
   validates :quality, presence: true
   validates :training_menu_id, presence: true
   validates :workout_details, presence: true
+
+  def reject_both_blank(attributes)
+    if attributes[:id]
+      attributes.merge!(_destroy: "1") if attributes[:weight].blank? and attributes[:reps].blank?
+      !attributes[:weight].blank? and attributes[:reps].blank?
+    else
+      attributes[:weight].blank? and attributes[:reps].blank?
+    end
+  end
 end
